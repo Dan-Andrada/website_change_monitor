d2438d826bd1cf02cfa7f80bf2f6960400c5ffb8
Andrada <elena.dan@student.upt.ro>
2025-11-15 22:21:25 +0200

    hash verification

--- internal/monitor/monitor.go
+++ internal/monitor/monitor.go
@@ -1,12 +1,18 @@
 package monitor
 
 import (
+	"crypto/sha256"
 	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"time"
 )
 
 type MonitorItem struct {
-	URL string `json:"url"`
+	URL      string `json:"url"`
+	LastHash string `json:"last_hash"`
 }
 
 func LoadMonitors() ([]MonitorItem, error) {
@@ -38,6 +44,85 @@ func AddURL(url string) error {
 		return err
 	}
 
+	for _, item := range items {
+		if item.URL == url {
+			return fmt.Errorf("URL already exists")
+		}
+	}
+
 	items = append(items, MonitorItem{URL: url})
 	return SaveMonitors(items)
+}
+
+func HashContent(content []byte) string {
+	h := sha256.Sum256(content)
+	return fmt.Sprintf("%x", h)
+}
+
+// vedem daca s a schimbat URL
+func CheckURL(item *MonitorItem) (bool, error) {
+	client := http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	req, err := http.NewRequest("GET", item.URL, nil)
+	if err != nil {
+		return false, err
+	}
+
+	req.Header.Set("User-Agent", "Go-Site-Monitor/1.0")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return false, err
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return false, err
+	}
+
+	newHash := HashContent(body)
+
+	if item.LastHash == "" {
+		item.LastHash = newHash
+		return false, nil // prima rulare
+	}
+
+	if newHash != item.LastHash {
+		item.LastHash = newHash
+		return true, nil
+	}
+
+	return false, nil
+}
+
+// verifica toate URL-urile si salveaza hash urile actualizate
+func CheckAll() ([]MonitorItem, error) {
+	items, err := LoadMonitors()
+	if err != nil {
+		return nil, err
+	}
+
+	changed := []MonitorItem{}
+
+	for i := range items {
+		isChanged, err := CheckURL(&items[i])
+		if err != nil {
+			fmt.Println("Eroare la verificarea:", items[i].URL, err)
+			continue
+		}
+		if isChanged {
+			changed = append(changed, items[i])
+		}
+	}
+
+	// salvez toate hash-urile actualizate
+	err = SaveMonitors(items)
+	if err != nil {
+		return nil, err
+	}
+
+	return changed, nil
 }
--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "website_change_monitor/cmd"
+import (
+	"fmt"
+	"os"
+	"website_change_monitor/internal/monitor"
+)
 
 func main() {
-	cmd.Execute()
+
+	if len(os.Args) > 1 && os.Args[1] == "add" && len(os.Args) > 2 {
+		url := os.Args[2]
+		err := monitor.AddURL(url)
+		if err != nil {
+			fmt.Println("Eroare la adăugarea URL:", err)
+		} else {
+			fmt.Println("URL adăugat cu succes:", url)
+		}
+		return
+	}
+
+	changed, err := monitor.CheckAll()
+	if err != nil {
+		fmt.Println("Eroare la verificarea site-urilor:", err)
+		return
+	}
+
+	if len(changed) == 0 {
+		fmt.Println("Nu s-au detectat schimbări.")
+	} else {
+		fmt.Println("Site-uri modificate:")
+		for _, item := range changed {
+			fmt.Println("-", item.URL)
+		}
+	}
 }

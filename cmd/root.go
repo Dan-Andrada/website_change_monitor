257fde7330cb3ae4571bb14b35f70c10dafbcc38
Andrada <elena.dan@student.upt.ro>
2026-01-12 15:45:03 +0200

    Implement before and after screenshots

--- cmd/root.go
+++ cmd/root.go
@@ -1,8 +1,56 @@
-// cmd/root.go
 package cmd
 
-import "website_change_monitor/internal/monitor"
+import (
+	"fmt"
+	"os"
+	"strconv"
 
-func Run() {
-	monitor.RunMonitorContinuously()
+	"website_change_monitor/internal/monitor"
+)
+
+func Execute() {
+	if len(os.Args) < 2 {
+		printHelp()
+		return
+	}
+
+	switch os.Args[1] {
+
+	case "add":
+		if len(os.Args) != 5 {
+			fmt.Println("Usage:")
+			fmt.Println("  gomonitor add <url> <selector> <frequency_minutes>")
+			return
+		}
+
+		freq, err := strconv.Atoi(os.Args[4])
+		if err != nil {
+			fmt.Println("Frequency must be a number (minutes)")
+			return
+		}
+
+		err = monitor.Add(
+			os.Args[2],
+			os.Args[3],
+			freq,
+		)
+		if err != nil {
+			fmt.Println("Error:", err)
+			return
+		}
+
+		fmt.Println("✅ Monitor added")
+
+	case "run":
+		monitor.RunMonitorContinuously()
+
+	default:
+		printHelp()
+	}
+}
+
+func printHelp() {
+	fmt.Println("Usage:")
+	fmt.Println("  gomonitor add <url> <selector> <frequency_minutes>")
+	fmt.Println("  gomonitor run")
 }
